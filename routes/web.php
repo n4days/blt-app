7fa62f5d1cd60b7d2ac2c7b0f577723646a6c117
n4days <alirsyhad.ramadhan49@gmail.com>
2026-02-04 11:22:48 +0700

    Feat: login menggunakan email
    Perbaruhi tampilan auth

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Auth\GoogleController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth');
 });
 Route::get('/auth/google', [GoogleController::class, 'redirect']);
 Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
